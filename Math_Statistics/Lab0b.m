16ea2b4f9f4cad50a87882369e06c2f7d621b92f
dstoiano <Byhenr.Yecdcdn>
2018-10-02 20:06:03 +0300

    update second part of lab 0

--- Math_Statistics/Lab0b.m
+++ Math_Statistics/Lab0b.m
@@ -3,7 +3,7 @@ clear
 
 
 g = 0.95;
-n = 10 ^ 6;
+n = 10 ^ 4;
 T = norminv((1 + g) / 2);
 
 
@@ -12,31 +12,39 @@ a = 2;
 s = sqrt(3/2);
 x = normrnd(a, s, n, 1);
 y = sqrt(3 * pi) * abs(x);
-int = mean(y)
+I = mean(y)
 d = T * std(y) / sqrt(n)
-I = [int - d; int + d]
+Int = [I - d, I + d]
+
+n = 10 ^ 6;
+a = 2;
+s = sqrt(3/2);
+x = normrnd(a, s, n, 1);
+y = sqrt(3 * pi) * abs(x);
+I = mean(y)
+d = T * std(y) / sqrt(n)
+Int = [I - d, I + d]
 
 format long
-I = quad('abs(x)*exp(-(x-2)^2/3)', -inf, inf)
+I_real = quad('abs(x)*exp(-(x-2)^2/3)', -inf, inf)
 
 
 
-fprintf('\n\nCalculate for x^3*exp(-x/3) from 0 to inf\n\n')
-l = 1/3;
-x = exprnd(l, n, 1);
-y = 3 * x.^3;
-int = mean(y)
-d = T * std(y) / sqrt(n)
-I = [int - d; int + d]
+fprintf('\n\nCalculate for (x+x.^2).^(1/3) from 1 to 3\n\n')
+n = 10 ^ 4;
+x = unifrnd(1, 3, n, 5);
+y = 2*(x+x.^2).^(1/3);
+I = mean(y)
+I1 = mean(I)
+d = T * std(y) / sqrt(n);
+d1 = mean(d)
+Int = [I1 - d1, I1 + d1]
 
-I = quad('x.^3*exp(-x/3)', 0, inf)
-
-
-fprintf('\n\nCalculate for \n\n')
+n = 10 ^ 6;
 x = unifrnd(1, 3, n, 1);
 y = 2*(x+x.^2).^(1/3);
-int = mean(y)
+I = mean(y)
 d = T * std(y) / sqrt(n)
-I = [int - d; int + d]
+Int = [I - d, I + d]
 
-I = quad('(x+x.^2).^(1/3)', 1, 3)
+I_real = quad('(x+x.^2).^(1/3)', 1, 3)
